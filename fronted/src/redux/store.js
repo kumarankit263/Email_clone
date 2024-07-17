e9af8aa81989606031a47a4fd5215d78399a20cd
Ankit Kumar <ankitiit263@gmail.com>
2024-07-17 15:26:21 +0530

    Update store.js

--- fronted/src/redux/store.js
+++ fronted/src/redux/store.js
@@ -1,13 +1,4 @@
-// import{configureStore} from "@reduxjs/toolkit";
-// // import { defaultMethod } from "react-router-dom/dist/dom";
-// import appSlice from "./appSlice";
 
-// const store=configureStore({
-//     reducer:{
-//         app:appSlice
-//     }
-// });
-// export default store;
 
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
